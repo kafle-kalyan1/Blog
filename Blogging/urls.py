0a8716b7470687724763d27914b38e763c1cff41
Kalyan <Kalyankafle123@gmail.com>
2023-06-12 09:23:39 +0545

    Vercel

--- Blogging/urls.py
+++ Blogging/urls.py
@@ -18,3 +18,5 @@ urlpatterns = [
 handler404 = handler404
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
