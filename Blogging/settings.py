bbdc9c9cd277c988b44b1d09228ebb8650383562
kalyan.kafle <kalyan.kafle@aegishms.com>
2023-07-06 23:21:47 +0545

    MORE

--- Blogging/settings.py
+++ Blogging/settings.py
@@ -167,8 +167,10 @@ USE_TZ = True
 
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiless_build', 'static')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 MEDIA_ROOT = ''
 MEDIA_URL = '/media/'
