9786d739a1fa02de567c0655fa6e17ca56c14f11
kalyan.kafle <kalyan.kafle@aegishms.com>
2023-07-07 00:13:34 +0545

    FINAL

--- Blogging/settings.py
+++ Blogging/settings.py
@@ -166,20 +166,9 @@ USE_I18N = True
 USE_TZ = True
 
 
-# STATIC_URL = 'static/'
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
-
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "/static/")
-
-STATICFILES_DIRS = (
-    STATIC_ROOT,
-) 
+STATIC_URL = 'static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 
 
